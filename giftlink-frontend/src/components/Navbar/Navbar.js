8e1dc2247b115ae6143d55163354a2a49632d29a
zapk13 <zapk13@gmail.com>
2025-03-02 14:37:16 -0500

    Updated files and debugging

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -14,9 +14,9 @@ export default function Navbar() {
                         <a className="nav-link" href="/app">Gifts</a> {/* Updated Link */}
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/app/search">Search</Link>
-                        </li>
-                        </ul>
+                        <a className="nav-link" href="/app/search">Search</a>
+                    </li>
+                </ul>
             </div>
         </nav>
     );
